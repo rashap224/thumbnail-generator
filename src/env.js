00e4ebbf6eedba42be06625ad1b944b217ba82ff
rashap224 <rashapofficial11@gmail.com>
2025-04-30 11:01:50 +0530

    some changes in env.js file

--- src/env.js
+++ src/env.js
@@ -7,16 +7,31 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    AUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    AUTH_DISCORD_ID: z.string(),
-    AUTH_DISCORD_SECRET: z.string(),
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    NEXTAUTH_SECRET:
+      process.env.NODE_ENV === "production"
+        ? z.string()
+        : z.string().optional(),
+    NEXTAUTH_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      (str) => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string() : z.string().url(),
+    ),
+    BASE_URL: z.string().url(),
+    MY_AWS_ACCESS_KEY: z.string(),
+    MY_AWS_SECRET_KEY: z.string(),
+    MY_AWS_REGION: z.string(),
+    MY_AWS_BUCKET_NAME: z.string(),
+    STRIPE_PUBLISHABLE_KEY: z.string(),
+    STRIPE_SECRET_KEY: z.string(),
+    STRIPE_WEBHOOK_SECRET: z.string(),
+    STRIPE_10_PACK: z.string(),
+    STRIPE_25_PACK: z.string(),
+    STRIPE_100_PACK: z.string(),
   },
 
   /**
@@ -33,11 +48,20 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    AUTH_SECRET: process.env.AUTH_SECRET,
-    AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
-    AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    BASE_URL: process.env.BASE_URL,
+    MY_AWS_ACCESS_KEY: process.env.MY_AWS_ACCESS_KEY,
+    MY_AWS_SECRET_KEY: process.env.MY_AWS_SECRET_KEY,
+    MY_AWS_REGION: process.env.MY_AWS_REGION,
+    MY_AWS_BUCKET_NAME: process.env.MY_AWS_BUCKET_NAME,
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    STRIPE_10_PACK: process.env.STRIPE_10_PACK,
+    STRIPE_25_PACK: process.env.STRIPE_25_PACK,
+    STRIPE_100_PACK: process.env.STRIPE_100_PACK,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
